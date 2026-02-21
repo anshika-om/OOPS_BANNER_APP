a65f864448a1c6ad3e91a1b528a7f627675f5425
anshika-om <ao3051@srmist.edu.in>
2026-02-21 13:50:51 +0530

    Coded to BannerApp ArrayLoop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,16 @@
 class OOPSBannerApp {
-
     public static void main(String[] args) {
-
-        // Inline Array Initialization
-        String[] banner = {
-            "OOOO   OOOOO  PPPP    SSSSS",
-            "O   O  O   O  P   P  S     ",
-            "O   O  O   O  PPPP    SSSSS ",
-            "O   O  O   O  P           S ",
-            "OOOO   OOOOO  P       SSSSS  "
-        };
-
-        // Printing Banner using loop
-        for (String line : banner) {
+        String[] lines = new String[9];        
+        lines[0] = String.join(" "," ***   ***  ****** ***** ");
+        lines[1] = String.join(" ","** ** ** ** **  ** ** ");
+        lines[2] = String.join(" ","** ** ** ** **  ** ** ");
+        lines[3] = String.join(" ","** ** ** ** **  ** ** ");
+        lines[4] = String.join(" ","** ** ** ** ******  ***");
+        lines[5] = String.join(" ","** ** ** ** **        ** ");
+        lines[6] = String.join(" ","** ** ** ** **        ** ");
+        lines[7] = String.join(" ","** ** ** ** **        ** ");
+        lines[8] = String.join(" "," ***   ***  **     ***** ");         
+        for (String line : lines) {
             System.out.println(line);
         }
     }
